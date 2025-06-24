c3b8ad378ef3a34e5a1a6c977b7921e1fe14619b
Alexander Ashwood <alexander.ashwood@sky.uk>
2025-06-24 10:17:11 +0100

    feat: configure basic proxy config

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -1,18 +1,32 @@
 import { fileURLToPath, URL } from 'node:url'
 
-import { defineConfig } from 'vite'
+import { defineConfig, mergeConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import { BASIC_CONF } from '../vite.config'
+
+const PROXY_CONF = {
+  target: 'http://localhost:3000',
+  secure: false,
+  changeOrigin: true,
+}
+
+const BASIC_PROXY_OBJ = {
+  '^/api/.*': PROXY_CONF,
+}
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [
-    vue(),
-    vueDevTools(),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+export default mergeConfig(
+  BASIC_CONF,
+  defineConfig({
+    plugins: [vue(), vueDevTools()],
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url)),
+      },
     },
-  },
-})
+    server: {
+      proxy: BASIC_PROXY_OBJ,
+    },
+  }),
+)
